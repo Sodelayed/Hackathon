d575380d3710229a1069979a494589a56cb27157
Ivanov Dmitriy <diver_space@mail.ruecho>
2023-09-03 00:44:58 +0300

    complit

--- src/menu.js
+++ src/menu.js
@@ -4,6 +4,7 @@ import { AsteroidEvasionModule } from "./modules/asteroidEvasion.module" // Им
 import { TicTacToeModule } from "./modules/tictactoe.module"
 import { SnakeGame } from "./modules/snakeGame"; // Импорт модуля игры Змейка
 import { DinoGameModule } from "./modules/dinoGame.module"
+
 export class ContextMenu extends Menu {
   
     constructor(selector) {
@@ -41,27 +42,8 @@ export class ContextMenu extends Menu {
         this.el.appendChild(liElement);
     }
 }
+const contextMenu = new ContextMenu('.menu')
 
-// Создание и инициализация контекстного меню
-const contextMenu = new ContextMenu('.menu');
-// Создание модуля для изменения фона и добавление его в контекстное меню
-const backgroundModule = new BackgroundModule();
-contextMenu.add(backgroundModule);
-// Создание модуля для подсчета кликов и добавление его в контекстное меню
-const clicksModule = new ClicksModule();
-contextMenu.add(clicksModule);
-// Создание модуля для генерации фигур и добавление его в контекстное меню
-const shapeModule = new ShapeModule();
-contextMenu.add(shapeModule);
-// Создание модуля для таймера и добавление его в контекстное меню
-const timerModule = new TimerModule();
-contextMenu.add(timerModule);
-// Создание модуля для случайного звука и добавление его в контекстное меню
-const randomSoundModule = new RandomSoundModule();
-contextMenu.add(randomSoundModule);
-// Создание модуля для кастомного сообщения и добавление его в контекстное меню
-const customMessageModule = new CustomMessageModule();
-contextMenu.add(customMessageModule);
 // Создание модуля для ракетной команды и добавление его в контекстное меню
 const missileCommandModule = new AsteroidEvasionModule();
 contextMenu.add(missileCommandModule);
