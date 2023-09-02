fea81ee0b9a8c33aa84f8ea05f662c28ba4782ff
Ivanov Dmitriy <diver_space@mail.ruecho>
2023-09-02 23:43:21 +0300

    подключил дино

--- src/menu.js
+++ src/menu.js
@@ -7,8 +7,8 @@ import { RandomSoundModule } from "./modules/sound.module"; // Импорт мо
 import { CustomMessageModule } from "./modules/customMessage.module" // Импорт модуля кастомного сообщения
 import { AsteroidEvasionModule } from "./modules/asteroidEvasion.module" // Импорт модуля игры про астероиды
 import { TicTacToeModule } from "./modules/tictactoe.module"
-import { SnakeGame } from "./modules/snakeGame.module"; // Импорт модуля игры Змейка
-
+import { SnakeGame } from "./modules/snakeGame"; // Импорт модуля игры Змейка
+import { DinoGameModule } from "./modules/dinoGame.module"
 export class ContextMenu extends Menu {
   
     constructor(selector) {
@@ -76,3 +76,6 @@ contextMenu.add(tictactoeModule)
 // Создание модуля для игры Змейка и добавление его в контекстное меню
 const snakeGameModule = new SnakeGame();
 contextMenu.add(snakeGameModule);
+
+const dinoGameModule = new DinoGameModule()
+contextMenu.add(dinoGameModule)
